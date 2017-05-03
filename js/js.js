fd6bf131ca4210817f9489cfe64f0efdc110d59f
SOSandreu1095 <sos.andreu.sos@gmail.com>
2017-05-03 22:10:01 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 /*Al cargar la pagina*/
 $(document).ready(function(){
 
-    $("h1").fadeIn(2000);
+    $("h1").fadeIn(1500);
   
     $(".not").mouseenter(function(){
         $("img", this).fadeTo(0.5,0.2);
@@ -11,5 +11,15 @@ $(document).ready(function(){
     $(".not").mouseleave(function(){
         $("img", this).fadeTo(0.5,1);
         $(".desc", this).fadeOut(200)
-    });    
+    });
+
+    //BOTON DE CARGAR NOTICIAS
+    $("#btnLoad").click(function() {
+        cargarNoticias();
+    });
 });
+
+
+function cargarNoticias(){
+    alert("CARGADAS");
+}
