ae87373e7a0ef599111aee9164a0102fa1d675ef
SOSandreu1095 <sos.andreu.sos@gmail.com>
2017-05-08 20:17:31 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -10,15 +10,8 @@ $(document).ready(function () {
 
     $("h1").fadeIn(1500);
 
-    $(".not").mouseenter(function () {
-        $("img", this).fadeTo(0.5, 0.2);
-        $(".desc", this).fadeIn(200);
-    });
+    cargarEfectos();
 
-    $(".not").mouseleave(function () {
-        $("img", this).fadeTo(0.5, 1);
-        $(".desc", this).fadeOut(200)
-    });
 
     //BOTON DE CARGAR NOTICIAS
     $("#btnLoad").click(function () {
@@ -34,6 +27,18 @@ $(document).ready(function () {
 });
 
 
+function cargarEfectos(){
+    $(".not").mouseenter(function () {
+        $("img", this).fadeTo(0.5, 0.2);
+        $(".desc", this).fadeIn(200);
+    });
+
+    $(".not").mouseleave(function () {
+        $("img", this).fadeTo(0.5, 1);
+        $(".desc", this).fadeOut(200)
+    });
+}
+
 
 function cargarNoticias() {
     if ((fichLoaded < fichName.length) && (cargarFichero(fichName[fichLoaded]))) {
@@ -44,7 +49,6 @@ function cargarNoticias() {
 }
 
 function cargarFichero(nombreFichero) {
-    alert(nombreFichero + " cargado");
     var path = "https://raw.githubusercontent.com/SOSandreu1095/WebNoticias/master/data/" + nombreFichero;
 
     //cargar Noticias
@@ -56,48 +60,41 @@ function cargarFichero(nombreFichero) {
 
 function crearNoticia(json, nombreFichero) {
 
-    var idRow = "row" + nombreFichero;
+    var m = document.getElementById("notCargar");
 
-    var row = $("<div></div>", { "id": idRow, "class": "row" });
-    var col = $("<div></div>", { "id": "col" + nombreFichero, "class": "col col-sm-6" });
-    var a = $("<a></a>", { "href": "#" });
+    var row = document.createElement("div");
+    row.id = "row" + nombreFichero;
+    row.className = "row";
+    m.appendChild(row);
 
+    for (i = 0; i < 2; i++) {
+        var col = document.createElement("div");
+        col.className = "col col-sm-6";
+        var a = document.createElement("a");
+        a.setAttribute('href', "#");
+        var h3 = document.createElement("h3");
+        h3.className = "notTitle";
+        h3.textContent = "TITLE NEW HERE";
+        var h5 = document.createElement("h5");
+        h5.className = "date";
+        h5.textContent = "12/12/12";
+        var n = document.createElement("div");
+        n.className = "not img-rounded";
+        var img = document.createElement("img");
+        img.src = "img/dropshot.jpg";
+        img.alt = "image New";
+        var des = document.createElement("p");
+        des.className = "desc";
+        des.textContent = "This is the fucking and awesome description of the fucking new :D";
 
-    $(col).append(a);
-    $(row).append(col);
-    $('#row').append(row);
-    /*
-    //Como hay 2 noticias por fichero:
-    for (i = 0; i < 1; i++) {
-
-        //Creamos la fila y la columna
-        var col = $("<div></div>", { "id": "col" + nombreFichero + i, "class": "col col-sm-6" });
-        var a = $("<a></a>", { "href": "#" });
-        var h3 = $("<h3></h3>", { "class": "notTitle", "text": "Titulo noticia" });
-        var h5 = $("<h5></h5>", { "class": "date", "text": "12/12/12" });
-        var n = $("<div></div>", { "class": "not img-rounded" });
-        var img = $("<img>", { "src": "img/dropshot.jpg", "alt": "imageNew" });
-        var p = $("<p></p>", { "class": "desc", "text": "AOFHEAKFHDKAFHJ HFAD FAJDHFKDF AGD AB " });
-
-        //$("#notCargar").append(row).children().append(col).children().append(a).children().append(h3).append(h5).append(n);
-
+        n.appendChild(img);
+        n.appendChild(des);
+        a.appendChild(h3);
+        a.appendChild(h5);
+        a.appendChild(n);
         col.appendChild(a);
         row.appendChild(col);
-        $('#row').append(row);
-        //append(n).children().append(img).append(p);
-    }
 
-    /*
-        <div class="row">
-            <div class="col col-sm-6">
-                <a href="news1.html">
-                    <h3 id="t1" class="notTitle">NUEVO CAMPEONATO</h3>
-                    <h5 class="date">30-07-2017</h5>
-                    <div id="not2" class="not img-rounded">
-                        <img src="img/dropshot.jpg" alt="dropshotIMG">
-                        <p id="d1" class="desc">Apúntate al nuevo torneo antes de que se acaben las plazas, premios para los 5 primeros equipos</p>
-                    </div>
-                </a>
-            </div>
-            */
+        cargarEfectos();
+    }
 }
